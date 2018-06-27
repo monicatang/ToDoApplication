2d655bc2554014bfa31c1f309eb1bcb97dc19bc3
Monica Tang <monicatang@fb.com>
2018-06-27 16:53:07 -0700

    add edit item feature

---
+++ app/src/main/java/me/monicatang/todoapplication/EditItemActivity.java
@@ -0,0 +1,36 @@
+package me.monicatang.todoapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import static me.monicatang.todoapplication.MainActivity.ITEM_POSITION;
+import static me.monicatang.todoapplication.MainActivity.ITEM_TEXT;
+
+public class EditItemActivity extends AppCompatActivity {
+    EditText etItemText;
+    int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+
+        etItemText = (EditText) findViewById(R.id.etItemText);
+        etItemText.setText(getIntent().getStringExtra(ITEM_TEXT));
+        position = getIntent().getIntExtra(ITEM_POSITION, 0);
+        getSupportActionBar().setTitle("Edit Item");
+
+
+    }
+
+    public void onSaveItem(View v){
+        Intent data = new Intent();
+        data.putExtra(ITEM_TEXT, etItemText.getText().toString());
+        data.putExtra(ITEM_POSITION, position);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
--- app/src/main/java/me/monicatang/todoapplication/MainActivity.java
+++ app/src/main/java/me/monicatang/todoapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package me.monicatang.todoapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,6 +19,11 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int EDIT_REQUEST_CODE = 20;
+
+    public static final String ITEM_TEXT = "itemText";
+    public static final String ITEM_POSITION = "itemPosition";
 
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
@@ -51,12 +57,23 @@ public class MainActivity extends AppCompatActivity {
     private void setupListViewListener() {
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long l) {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 items.remove(position);
                 itemsAdapter.notifyDataSetChanged();
                 Log.i("MainActivity", "Removed item " + position);
                 writeItems();
                 return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                i.putExtra(ITEM_TEXT, items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+
+                startActivityForResult(i, EDIT_REQUEST_CODE);
             }
         });
     }
@@ -79,6 +96,20 @@ public class MainActivity extends AppCompatActivity {
             FileUtils.writeLines(getDataFile(), items);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            int position = data.getExtras().getInt(ITEM_POSITION, 0);
+            items.set(position, updatedItem);
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+            Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show();
         }
     }
 }
